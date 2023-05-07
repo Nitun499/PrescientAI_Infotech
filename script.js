0ff959ff80558529549db86d45f3649c32e69049
Nitun499 <84559326+Nitun499@users.noreply.github.com>
2023-05-07 18:01:03 +0530

    code for website

---
+++ script.js
@@ -0,0 +1,6 @@
+// var myCarousel = document.querySelector('#carouselExampleIndicators')
+//   var carousel = new bootstrap.Carousel(myCarousel, {
+//     interval: 3000, // Change interval time in ms here
+//     pause: false
+//   })
+
